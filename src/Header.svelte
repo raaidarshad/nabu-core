32c5351ebcb96139c037bb7de81873c836b8e025
Raaid <raaid@protonmail.com>
2021-03-29 10:41:42 -0400

    Add a "make this your home page" link

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,9 +1,20 @@
 <main>
     <div>
-        <a class="aboutlink" href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <p class="alignleft">
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://support.mozilla.org/en-US/kb/how-to-set-the-home-page"
+                >Make this your home page</a
+            >
+        </p>
+        <p class="alignright">
+            <a class="aboutlink" href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </p>
     </div>
 </main>
+
 <style>
     div {
         text-align: right;
@@ -12,8 +23,16 @@
     a {
         color: #555555;
     }
-    
+
     .aboutlink {
         margin-right: 1em;
+    }
+
+    .alignleft {
+        float: left;
+    }
+
+    .alignright {
+        float: right;
     }
 </style>
