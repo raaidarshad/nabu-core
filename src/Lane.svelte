22ae3d1b16190bfb5638212b70925c33611020ca
Raaid <raaid@protonmail.com>
2021-04-05 10:22:03 -0400

    add dropdown arrow to source selectors

--- src/Lane.svelte
+++ src/Lane.svelte
@@ -47,13 +47,20 @@
 
 <style>
     select {
-        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        background: transparent;
+        background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+        background-repeat: no-repeat;
+        background-position-x: 100%;
+        background-position-y: 5px;
         border: none;
         margin: 0;
-        width: 100%;
-        text-align: center;
         margin-top: 10px;
+        padding-right: 20px;
         appearance: none;
+        text-align: center;
+        width: 100%;
     }
 
     select:hover {
