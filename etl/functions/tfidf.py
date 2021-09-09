8e70ad58887b6346bf6a37b2db161169bb3487c1
Raaid <raaid@protonmail.com>
2021-09-09 15:35:42 +0100

    add test_tfidf.py and fill it out

--- etl/functions/tfidf.py
+++ etl/functions/tfidf.py
@@ -1,6 +1,26 @@
 from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import TfidfTransformer
 
+from etl.functions.counts import CountData
+
+
+class SimilarityData(CountData):
+    tfidf_matrix: csr_matrix
+    similarity_matrix: csr_matrix
+
+
+def compute_similarity_data(count_data: CountData) -> SimilarityData:
+    # a csr_matrix with the same col/row indices but the values are tfidf, so same shape diff vals
+    tfidfs = compute_tfidf(count_data.count_matrix)
+    # was N x M, now N x N where value at (i, j) shows the similarity between article i and j
+    similarities = compute_similarities(tfidfs)
+
+    return SimilarityData(
+        tfidf_matrix=tfidfs,
+        similarity_matrix=similarities,
+        **count_data.__dict__
+    )
+
 
 def compute_tfidf(counts: csr_matrix) -> csr_matrix:
     # takes the input count matrix and computes the corresponding tfidf matrix
@@ -18,14 +38,3 @@ def filter_similarities(similarities: csr_matrix, threshold: float) -> csr_matri
     # returns the cosines sparse matrix but only includes elements that are above the specified threshold. Keeps same
     # shape
     return similarities.multiply(similarities >= threshold)
-
-
-def main():
-    # MIGHT NEED STUFF FROM counts.py
-    # TF IDF
-    tfidfs = compute_tfidf(sparse_counts)
-    # now it is a csr_matrix with the same col/row indices but the values are tfidf, so same shape diff vals
-    similarities = compute_similarities(tfidfs)
-    # was N x M, now N x N where value at (i, j) shows the similarity between article i and j
-    filtered = filter_similarities(similarities, 0.45)
-    # removes values from the sparse matrix that are below the similarity value
