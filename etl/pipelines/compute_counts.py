133ab76eca21ccf1747a45cc343e570bd9a575e6
Raaid <raaid@protonmail.com>
2021-09-20 14:04:46 +0100

    add cloud db resource/mode to compute_counts pipeline

--- etl/pipelines/compute_counts.py
+++ etl/pipelines/compute_counts.py
@@ -3,8 +3,14 @@ from datetime import datetime, timedelta
 from dagster import AssetKey, EventLogEntry, ModeDefinition, PresetDefinition, RunRequest, \
     SensorEvaluationContext, asset_sensor, pipeline
 
-from etl.resources.database_client import local_database_client, compute_counts_test_database_client
+from etl.resources.database_client import cloud_database_client, local_database_client, \
+    compute_counts_test_database_client
 from etl.solids.compute_counts import get_articles, compose_rows, compute_count_matrix, load_counts
+
+
+cloud_resource_defs = {
+    "database_client": cloud_database_client
+}
 
 local_resource_defs = {
     "database_client": local_database_client
@@ -15,6 +21,7 @@ test_resource_defs = {
 }
 
 # modes
+cloud_mode = ModeDefinition(name="cloud", resource_defs=cloud_resource_defs)
 local_mode = ModeDefinition(name="local", resource_defs=local_resource_defs)
 test_mode = ModeDefinition(name="test", resource_defs=test_resource_defs)
 
@@ -23,14 +30,14 @@ my_threshold = str(my_threshold)
 
 # presets
 main_preset = PresetDefinition(
-    mode="test",
+    mode="local",
     name="main_preset",
     run_config={"solids": {"get_articles": {"config": {"time_threshold": my_threshold}}}}
 )
 
 
 # sensors
-@asset_sensor(asset_key=AssetKey("article_table"), pipeline_name="compute_counts", mode="local")
+@asset_sensor(asset_key=AssetKey("article_table"), pipeline_name="compute_counts", mode="cloud")
 def article_table_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
     yield RunRequest(
         run_key=context.cursor,
@@ -47,7 +54,7 @@ def article_table_sensor(context: SensorEvaluationContext, asset_event: EventLog
     )
 
 
-@pipeline(mode_defs=[local_mode, test_mode], preset_defs=[main_preset], tags={"table": "count"})
+@pipeline(mode_defs=[cloud_mode, local_mode, test_mode], preset_defs=[main_preset], tags={"table": "count"})
 def compute_counts():
     articles = get_articles()
     count_matrix, features = compute_count_matrix(articles=articles)
