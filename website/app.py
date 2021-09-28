c14deda3b987f5bf5ccef77a78d76b5fda0a858d
Raaid <raaid@protonmail.com>
2021-09-28 14:25:00 +0100

    get topics endpoint to provide all info, including articles and sources (makes it a way heavier response, would love to slim it down to only the bits I need)

--- website/app.py
+++ website/app.py
@@ -7,7 +7,6 @@ from sqlmodel import Session, create_engine, desc, func, select
 
 from ptbmodels.models import Cluster, ClusterToLink
 
-
 app = FastAPI()
 
 
@@ -27,13 +26,18 @@ def default_feed(rssUrl: str, limit: int):
     return feed
 
 
-@app.get("/topics", response_model=list[Cluster])
-def topics(limit: int = 10, db_client: Session = Depends(get_db_client)):
+@app.get("/topics")
+def topics(limit: int = 5, db_client: Session = Depends(get_db_client)):
     statement1 = select(ClusterToLink.cluster_id, func.count(ClusterToLink.article_id).label("size")). \
         group_by(ClusterToLink.cluster_id).order_by(desc("size")).limit(limit)
     sub1 = statement1.subquery("s1")
     statement2 = select(Cluster).join(sub1).order_by(desc("size"))
-    return db_client.exec(statement2).all()
+    clusters = db_client.exec(statement2).all()
+    # sadly seem to need to manually include articles (and their sources) into the response
+
+    clusters = [{**c.dict(), "articles": [{**a.dict(), "source": a.source} for a in c.articles]} for c in clusters]
+
+    return clusters
 
 
 # Path for all the static files (compiled JS/CSS, index.html, etc.)
