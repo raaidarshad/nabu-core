df6e35bdaf80288f67aa33e564c42ed556419b9a
Raaid <raaid@protonmail.com>
2021-09-30 15:14:57 +0100

    change schedule settings for extract articles

--- etl/pipelines/extract_articles.py
+++ etl/pipelines/extract_articles.py
@@ -73,14 +73,11 @@ def extract_articles():
     load_articles(articles)
 
 
-freq = 15  # minutes
-
-
-# every 15 minutes
-@schedule(cron_schedule=f"*/{freq} * * * *", pipeline_name="extract_articles", mode="cloud")
+# every hour at the 13th minute (just picked some non-zero minute so it isn't on the hour out of respect to the
+# urls I am hitting)
+@schedule(cron_schedule=f"13 */1 * * *", pipeline_name="extract_articles", mode="cloud")
 def main_schedule(context: ScheduleExecutionContext):
-    # it seems I've been missing articles, so going to have this grab everything the last few hours in case
-    # the publishers set the "published at" time as earlier
+    # 4 hour span in case the rss feeds have articles that are back-published
     raw_threshold = context.scheduled_execution_time - timedelta(hours=4)
     if not raw_threshold.tzinfo:
         raw_threshold = raw_threshold.astimezone(tz=timezone.utc)
