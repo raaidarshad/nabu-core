754cbfa8ac98de37036611e09166c46649cc1df2
Raaid <raaid@protonmail.com>
2021-10-01 10:48:12 +0100

    add file skeletons for refactored article pipelines

---
+++ etl/pipelines/extract_article_metadata.py
@@ -0,0 +1,11 @@
+from dagster import ModeDefinition, PresetDefinition, ScheduleExecutionContext, pipeline, schedule
+
+# resource definitions
+
+# mode definitions
+
+# preset definitions
+
+# pipelines
+
+# schedules/sensors
