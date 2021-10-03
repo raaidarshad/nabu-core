45952dc3472635a4e156c60232f7091d1205fe7f
Raaid <raaid@protonmail.com>
2021-10-03 13:36:27 +0100

    source and rssfeed loads work, look good locally

--- etl/pipelines/load_sources.py
+++ etl/pipelines/load_sources.py
@@ -2,7 +2,8 @@ from dagster import ModeDefinition, PresetDefinition, pipeline
 
 from etl.resources.database_client import cloud_database_client, cloud_database_engine, local_database_client, \
     local_database_engine, mock_database_client, mock_database_engine
-from etl.solids.load_sources import create_tables, get_entities_from_file, load_source_rows, load_rss_feed_rows
+from etl.solids.load_sources import create_tables, get_rss_feeds_from_file, get_sources_from_file, load_rss_feed_rows, \
+    load_source_rows
 
 
 # resources
@@ -35,6 +36,13 @@ main_preset = PresetDefinition(
 @pipeline(mode_defs=[cloud_mode, local_mode, test_mode], preset_defs=[main_preset], tags={"table": "source"})
 def load_sources():
     path_format = create_tables()
-    sources, rss_feeds = get_entities_from_file(filepath=path_format)
+    sources = get_sources_from_file(path_format=path_format)
     load_source_rows(sources)
+
+
+@pipeline(mode_defs=[cloud_mode, local_mode, test_mode], preset_defs=[main_preset], tags={"table": "rssfeed"})
+def load_rss_feeds():
+    path_format = create_tables()
+    rss_feeds = get_rss_feeds_from_file(path_format=path_format)
     load_rss_feed_rows(rss_feeds)
+
--- etl/solids/load_sources.py
+++ etl/solids/load_sources.py
@@ -8,18 +8,20 @@ from etl.common import Context
 from ptbmodels.models import RssFeed, Source
 
 
-@solid
-def get_entities_from_file(context: Context, path_format: str):
-    context.log.debug("Loading source and rss_feed from JSON on disk")
-    # load sources
-    j_sources = _load_json(filepath=path_format.format(table_name="source"))
-    # load rss feeds
-    j_rss_feeds = _load_json(filepath=path_format.format(table_name="rssfeed"))
-    # emit entities
-    sources = [Source(**src) for src in j_sources]
-    rss_feeds = [RssFeed(**rss) for rss in j_rss_feeds]
+def get_entities_from_file_factory(name: str, entity_type, **kwargs):
+    @solid(name=name, **kwargs)
+    def _load_rows_solid(context: Context, path_format: str):
+        context.log.debug(f"Loading {entity_type.__name__} from JSON file")
+        # load entities
+        entities = _load_json(filepath=path_format.format(table_name=entity_type.__tablename__))
+        # emit entities
+        # entities = [entity_type(**e) for e in j_entities]
+        return entities
+    return _load_rows_solid
 
-    return sources, rss_feeds
+
+get_sources_from_file = get_entities_from_file_factory("get_sources_from_file", Source)
+get_rss_feeds_from_file = get_entities_from_file_factory("get_rssfeeds_from_file", RssFeed)
 
 
 @solid(required_resource_keys={"database_engine"})
