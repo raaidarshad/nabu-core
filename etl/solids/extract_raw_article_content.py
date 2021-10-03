6db493f5a6ccabe170aa6ac7782bc2d8768f819c
Raaid <raaid@protonmail.com>
2021-10-03 15:13:53 +0100

    fill out get_articles solid for raw article content pipeline

--- etl/solids/extract_raw_article_content.py
+++ etl/solids/extract_raw_article_content.py
@@ -1,11 +1,24 @@
-from dagster import String, solid
+import concurrent.futures
+import requests
 
-from etl.common import DagsterTime, Context
+from dagster import solid
+from sqlmodel import Session, select
+
+from etl.common import DagsterTime, Context, load_rows_factory, str_to_datetime
 from ptbmodels.models import Article, RawContent
+
 
 @solid(required_resource_keys={"database_client"}, config_schema={"begin": DagsterTime, "end": DagsterTime})
 def get_articles(context: Context) -> list[Article]:
-    ...
+    db_client: Session = context.resources.database_client
+    begin = str_to_datetime(context.solid_config["begin"])
+    end = str_to_datetime(context.solid_config["end"])
+
+    statement = select(Article).where((begin <= Article.added_at) & (Article.added_at <= end))
+    context.log.debug(f"Attempting to execute: {statement}")
+    articles = db_client.exec(statement).all()
+    context.log.debug(f"Got {len(articles)} articles")
+    return articles
 
 
 @solid(required_resource_keys={"http_client"})
@@ -13,6 +26,4 @@ def request_raw_content(context: Context, articles: list[Article]) -> RawContent
     ...
 
 
-@solid(required_resource_keys={"database_client"}, config_schema={"runtime": DagsterTime})
-def load_raw_content(context: Context, raw_content: list[RawContent]):
-    ...
+load_raw_content = load_rows_factory("load_raw_content", RawContent, ["article_id"])
