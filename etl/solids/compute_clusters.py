c9259ddb2223e72bb1d9274e480e2ec7e7801c2d
Raaid <raaid@protonmail.com>
2021-10-12 13:18:09 +0100

    first pass at updating compute_clusters solids

--- etl/solids/compute_clusters.py
+++ etl/solids/compute_clusters.py
@@ -1,58 +1,134 @@
-from dagster import AssetMaterialization, Float, Int, Output, solid
-from datetime import datetime, timedelta, timezone
-from sqlmodel import Session
+from collections import defaultdict
 
-from etl.common import Context
-from etl.functions.counts import CountData, get_count_data
-from etl.functions.tfidf import SimilarityData, compute_similarity_data
-from etl.functions.clusters import compute_cluster_data
+from dagster import Dict, Enum, EnumValue, Field, solid
+from pydantic import BaseModel
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import breadth_first_order
+from sklearn.base import BaseEstimator, ClusterMixin
+from sklearn.cluster import AgglomerativeClustering, DBSCAN, OPTICS
+from sklearn.feature_extraction.text import TfidfTransformer
 
-
-# want to do this with various clustering methods; do we want to store similarity data as
-# an intermediate piece? or we can wrap various clustering methods into solids,
-# then create specific pipelines and configs. needs some thinking/writing
-
-# get count terms
-# compute similarity
-# compute article clusters
-# load article clusters
+from etl.common import Context, DagsterTime, get_rows_factory, load_rows_factory, str_to_datetime
+from ptbmodels.models import Article, ArticleCluster, TermCount
 
 
-@solid(required_resource_keys={"database_client"}, config_schema={"minute_span": Int})
-def get_counts(context: Context) -> CountData:
-    db_client: Session = context.resources.database_client
-    minute_span = context.solid_config["minute_span"]
-    now = datetime.now(tz=timezone.utc)
-    threshold = now - timedelta(minutes=minute_span)
-    return get_count_data(datetime_threshold=threshold, db_client=db_client)
+# intermediate models
+class TFIDF(BaseModel):
+    tfidf: csr_matrix
+    counts: csr_matrix
+    index_to_article: list[Article]
+    index_to_term: list[str]
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
-@solid(config_schema={"filter_threshold": Float})
-def compute_similarity(context: Context, count_data: CountData) -> SimilarityData:
-    filter_threshold = context.solid_config["filter_threshold"]
-    return compute_similarity_data(count_data=count_data, filter_threshold=filter_threshold)
+# custom clustering models
+class PTB0(ClusterMixin, BaseEstimator):
+    def __init__(self, threshold=0.4, min_cluster_size=None):
+        self.threshold = threshold
+        self.min_cluster_size = min_cluster_size
+
+    def fit(self, X: csr_matrix, y=None, sample_weight=None):
+        # dot product of X with its transpose to get a cosine-similarity matrix,
+        # where the value at [i, j] tells you the cosine similarity between
+        # article i and article j
+        similarities = X.dot(X.transpose())
+        # filter to articles above a certain threshold of similarity
+        filtered = similarities.multiply(similarities >= self.threshold)
+        clusters = [
+            frozenset(breadth_first_order(filtered, idx, directed=False, return_predecessors=False)) for idx in
+            range(filtered.shape[0])]
+
+        self.labels_ = self._encode_to_int(clusters)
+        return self
+
+    @staticmethod
+    def _encode_to_int(seq):
+        cmap = {item: idx for idx, item in enumerate(set(seq))}
+        return [cmap[item] for item in seq]
 
 
-@solid(required_resource_keys={"database_client"}, config_schema={"minute_span": Int})
-def compute_and_load_clusters(context: Context, similarity_data: SimilarityData):
-    minute_span = context.solid_config["minute_span"]
-    db_client: Session = context.resources.database_client
-    compute_cluster_data(similarity_data, datetime.now(tz=timezone.utc), minute_span, db_client)
-    yield AssetMaterialization(asset_key="cluster_table", description="New rows added to cluster table")
-    # yield a dummy output, don't have access to the rows to add right here
-    yield Output(1)
+get_term_counts = get_rows_factory("get_term_counts", TermCount)
 
 
-# good, modular solids to have here for a variety of clustering approaches
+@solid
+def compute_tfidf(context: Context, term_counts: list[TermCount]) -> TFIDF:
+    # recreate count matrix and keep track of index_to_id and index_to_term
+    counts, articles, terms = zip(*[(tc.count, tc.article, tc.term) for tc in term_counts])
+    index_articles, index_to_article = _numerify(articles)
+    index_terms, index_to_term = _numerify(terms)
+    sparse_counts = csr_matrix((counts, (index_articles, index_terms)))
 
-# get_term_counts
+    # create tfidf matrix
+    tfidf = TfidfTransformer().fit_transform(sparse_counts)
 
-# compute_tfidf
-# compute_tfidf_similarity (rename? compute_cosines?)
+    # create intermediate object
+    return TFIDF(
+        tfidf=tfidf,
+        counts=sparse_counts,
+        index_to_article=index_to_article,
+        index_to_term=index_to_term
+    )
 
-# compute_bfs_clusters
-# compute_heirarchical_clusters
-# compute_dbscan_clusters
-# compute_optical_clusters
 
-# load_article_clusters
+def _numerify(target: list) -> tuple[list, list]:
+    deduped = list(set(target))
+    target_to_index = {t: idx for idx, t in enumerate(deduped)}
+    return [target_to_index[t] for t in target], deduped
+
+
+ClusterDenum = Enum("ClusterDenum",
+                    [EnumValue("Agglomerative"), EnumValue("DBSCAN"), EnumValue("OPTICS"), EnumValue("PTB0")])
+
+ClusterDenumConfig = Field(
+    config=ClusterDenum,
+    default_value="OPTICS",
+    is_required=False
+)
+
+
+@solid(config_schema={"runtime": DagsterTime,
+                      "cluster_type": ClusterDenumConfig,
+                      "cluster_parameters": Field(config=Dict, default_value={}, is_required=False),
+                      "begin": DagsterTime,
+                      "end": DagsterTime})
+def compute_article_clusters(context: Context, tfidf: TFIDF) -> list[ArticleCluster]:
+    cluster_type: str = context.solid_config["cluster_type"]
+    cluster_parameters: dict = context.solid_config["cluster_parameters"]
+
+    # get correct class based on input
+    cluster_method = {
+        "Agglomerative": AgglomerativeClustering,
+        "DBSCAN": DBSCAN,
+        "OPTICS": OPTICS,
+        "PTB0": PTB0
+    }[cluster_type]
+
+    # compute clusters given the specified cluster type and parameters
+    clustering = cluster_method(**cluster_parameters).fit(tfidf.tfidf)
+    # transform [0 0 1 1] to [(0, [0, 1]), (1, [2, 3])] so we know which row
+    # indices are in the same cluster, allowing us to reference the correct
+    # articles when adding the ArticleCluster to the DB
+    clusters_and_rows = _get_indices(clustering.labels_)
+
+    return [
+        ArticleCluster(
+            type=cluster_type,
+            parameters=cluster_parameters,
+            begin=str_to_datetime(context.solid_config["begin"]),
+            end=str_to_datetime(context.solid_config["end"]),
+            added_at=str_to_datetime(context.solid_config["runtime"]),
+            articles=[tfidf.index_to_article[idx] for idx in rows]
+        ) for _, rows in clusters_and_rows
+    ]
+
+
+def _get_indices(seq):
+    tally = defaultdict(list)
+    for idx, item in enumerate(seq):
+        tally[item].append(idx)
+    return ((key, locs) for key, locs in tally.items() if len(locs) > 1)
+
+
+load_article_clusters = load_rows_factory("load_article_clusters", ArticleCluster, [ArticleCluster.id])
