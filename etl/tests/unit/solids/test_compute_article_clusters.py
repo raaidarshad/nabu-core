0e2ddc9ae82fce2773d1550a1eee4954c98218e1
Raaid <raaid@protonmail.com>
2021-10-12 13:43:34 +0100

    add test_load_article_clusters

--- etl/tests/unit/solids/test_compute_article_clusters.py
+++ etl/tests/unit/solids/test_compute_article_clusters.py
@@ -5,8 +5,9 @@ from dagster import ModeDefinition, ResourceDefinition, SolidExecutionResult, ex
 
 from etl.common import datetime_to_str, get_current_time
 from etl.resources.database_client import mock_database_client
-from etl.solids.compute_article_clusters import get_term_counts
-from ptbmodels.models import TermCount, ArticleCluster
+from etl.solids.compute_article_clusters import compute_article_clusters, compute_tfidf, get_term_counts, \
+    load_article_clusters
+from ptbmodels.models import Article, ArticleCluster, TermCount
 
 
 def test_get_term_counts():
@@ -45,4 +46,45 @@ def test_compute_article_clusters():
 
 
 def test_load_article_clusters():
-    ...
+    article_clusters = [
+        ArticleCluster(
+            id=uuid4(),
+            type=f"type{idx}",
+            parameters=idx,
+            begin=get_current_time(),
+            end=get_current_time(),
+            added_at=get_current_time(),
+            articles=[Article(id=uuid4(),
+                              source_id=jdx,
+                              url=f"https://fake.com/article{jdx}",
+                              summary=f"summary{jdx}",
+                              title=f"title{jdx}",
+                              published_at=get_current_time(),
+                              added_at=get_current_time()
+                              ) for jdx in range(2)]
+        ) for idx in range(2)
+    ]
+
+    db = mock_database_client()
+
+    def _test_db_client(_init_context):
+        db.exec = Mock(return_value=1)
+        db.commit = Mock(return_value=1)
+        a = Mock()
+        a.count = Mock(return_value=1)
+        db.query = Mock(return_value=a)
+        return db
+
+    result: SolidExecutionResult = execute_solid(
+        load_article_clusters,
+        mode_def=ModeDefinition(name="test",
+                                resource_defs={"database_client": ResourceDefinition(_test_db_client)}),
+        input_values={"entities": article_clusters}
+    )
+
+    assert result.success
+    assert result.output_value() == article_clusters
+    # count of rows in mock should be the same, therefore no asset should be materialized
+    assert result.materializations_during_compute == []
+    assert db.exec.called_once()
+    assert db.commit.called_once()
