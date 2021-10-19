5d5114083d9d8f29134d9d3dcec0d8d4fe6a75c0
Raaid <raaid@protonmail.com>
2021-10-19 15:29:30 +0100

    add try/except with additional logging in _raw_to_parsed in transform_parsed_article_content.py

--- etl/solids/transform_parsed_article_content.py
+++ etl/solids/transform_parsed_article_content.py
@@ -12,15 +12,18 @@ def parse_raw_content(context: Context, raw_content: list[RawContent]) -> list[P
     runtime = str_to_datetime(context.solid_config["runtime"])
     parser: BaseParser = context.resources.html_parser
 
-    # TODO we probably want a try statement, but don't know what errors to look for yet
-    # very likely we'd want the try/except IN the actual parser resource
     def _raw_to_parsed(raw: RawContent):
-        return ParsedContent(
-            article_id=raw.article_id,
-            # don't think this will work since we're not in the same scope as when we pull the rows
-            # the first time; if it does, great! if it doesn't, can intentionally join to get data
-            content=parser.extract(raw.content, raw.article.rss_feed.parser_config),
-            added_at=runtime)
+        try:
+            return ParsedContent(
+                article_id=raw.article_id,
+                # don't think this will work since we're not in the same scope as when we pull the rows
+                # the first time; if it does, great! if it doesn't, can intentionally join to get data
+                content=parser.extract(raw.content, raw.article.rss_feed.parser_config),
+                added_at=runtime)
+        except AttributeError as e:
+            context.log.error(
+                f"Article id {raw.article_id} with parser_config {raw.article.rss_feed.parser_config} failed to parse.")
+            raise e
 
     # filter out None values for when we have a try/except statement implemented
     return list(filter(None, [_raw_to_parsed(raw) for raw in raw_content]))
