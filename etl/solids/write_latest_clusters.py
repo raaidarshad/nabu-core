33d97849eae810ee83c030d79fe742b5bf7bccbd
Raaid <raaid@protonmail.com>
2021-12-08 10:11:43 +0000

    update query and prepping function to account for pulling the number of sources per cluster

--- etl/solids/write_latest_clusters.py
+++ etl/solids/write_latest_clusters.py
@@ -1,7 +1,7 @@
 import json
 
 from dagster import Field, Int, solid
-from sqlmodel import Session, desc, func, select
+from sqlmodel import Session, desc, column, func, select
 
 from etl.common import Context
 from ptbmodels.models import ArticleCluster, ArticleClusterLink
@@ -11,7 +11,7 @@ ClusterLimit = Field(config=Int, default_value=10, is_required=False)
 
 
 @solid(required_resource_keys={"database_client"}, config_schema={"cluster_limit": ClusterLimit})
-def get_latest_clusters(context: Context) -> list[ArticleCluster]:
+def get_latest_clusters(context: Context) -> list[(ArticleCluster, int)]:
     db_client: Session = context.resources.database_client
 
     statement1 = select(
@@ -21,7 +21,7 @@ def get_latest_clusters(context: Context) -> list[ArticleCluster]:
         order_by(desc("size"))
     sub1 = statement1.subquery("s1")
     sub2 = select(func.max(ArticleCluster.added_at)).scalar_subquery()
-    statement2 = select(ArticleCluster).join(sub1).where(ArticleCluster.added_at == sub2).order_by(desc("size")).limit(context.solid_config["cluster_limit"])
+    statement2 = select(ArticleCluster, column("size")).join(sub1).where(ArticleCluster.added_at == sub2).order_by(desc("size")).limit(context.solid_config["cluster_limit"])
 
     context.log.info(f"Attempting to execute: {statement2}")
     entities = db_client.exec(statement2).all()
@@ -30,17 +30,18 @@ def get_latest_clusters(context: Context) -> list[ArticleCluster]:
 
 
 @solid
-def prep_latest_clusters(context: Context, clusters: list[ArticleCluster]) -> dict:
+def prep_latest_clusters(context: Context, clusters: list[(ArticleCluster, int)]) -> dict:
     prepped_clusters = [
         {
-            "topics": [{"term": k.term, "weight": k.weight} for k in c.keywords],
-            "articles": [{"title": a.title, "url": a.url, "source": a.source.name, "date": a.published_at.strftime("%d-%m-%Y")} for a in c.articles]
+            "topics": [{"term": k.term, "weight": k.weight} for k in c[0].keywords],
+            "articles": [{"title": a.title, "url": a.url, "source": a.source.name, "date": a.published_at.strftime("%d-%m-%Y")} for a in c[0].articles],
+            "source_count": c[1]
         }
         for c in clusters
     ]
 
     return {
-        "added_at": clusters[0].added_at,
+        "added_at": clusters[0][0].added_at,
         "clusters": prepped_clusters
     }
 
