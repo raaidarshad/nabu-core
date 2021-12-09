edb54becf503cabb1a3654ee272072b9f7bcc21f
Raaid <raaid@protonmail.com>
2021-12-09 10:38:18 +0000

    update boto client to pull all credentials from environment variables

--- etl/resources/boto_client.py
+++ etl/resources/boto_client.py
@@ -10,11 +10,11 @@ from botocore.client import BaseClient
 @resource
 def boto_client(init_context) -> BaseClient:
     session = boto3.session.Session()
-    return session.client('s3',
-                          region_name='nyc3',
-                          endpoint_url='https://nyc3.digitaloceanspaces.com',
-                          aws_access_key_id=os.getenv('SPACES_KEY'),
-                          aws_secret_access_key=os.getenv('SPACES_SECRET'))
+    return session.client("s3",
+                          region_name=os.getenv("SPACES_REGION", "nyc3"),
+                          endpoint_url=os.getenv("SPACES_ENDPOINT", "https://nyc3.digitaloceanspaces.com"),
+                          aws_access_key_id=os.getenv("SPACES_KEY"),
+                          aws_secret_access_key=os.getenv("SPACES_SECRET"))
 
 
 def mock_boto_client(init_context) -> BaseClient:
