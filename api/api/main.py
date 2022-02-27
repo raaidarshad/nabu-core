5bbfbe98d673121df23986688483317014c3db9e
Raaid <raaid@protonmail.com>
2022-02-27 17:24:54 +0000

    Extension server (#65)
    * update gitignore to ignore __pycache__
    
    * add api server first pass
    
    * create dockerfile for api server
    
    * add pytest and add 3 test cases for the api
    
    * rename top level dir to api instead of public-server, add blank files for ci and cd
    
    * fix tests to use fastapi/starlette test client, fill out api_ci.yaml
    
    * use `like` instead of `ilike` in search query, we don't expect capitals anywhere so we don't need the case insensitive comparison. `like` seems to run faster than `ilike` as well
    
    * fill out api_cd.yaml
    
    * update poetry version used in api_ci.yaml

---
+++ api/api/main.py
@@ -0,0 +1,56 @@
+import os
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import psycopg
+from psycopg.rows import dict_row
+from pydantic import BaseModel
+
+
+class SearchBody(BaseModel):
+    url: str
+    limit: Optional[int] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# endpoint to submitSearch (lets just call it search)
+@app.post("/search")
+async def submit_search(search_body: SearchBody):
+    # if the url is not found, return a 404 code
+    # if the url is found but no similar articles, return a 200 with an empty body
+    # if the url is found and there are similar articles, return a 200 and a full body
+    async with await psycopg.AsyncConnection.connect(os.getenv("DB_CONNECTION_STRING")) as aconn:
+        async with aconn.cursor(row_factory=dict_row) as acur:
+            await acur.execute("SELECT * FROM article WHERE url like %s", (search_body.url, ))
+            found_article = await acur.fetchone()
+            if found_article:
+                await acur.execute("""
+                    WITH onedayclusters AS (SELECT *
+                        FROM articleclusterlink acl1
+                                 JOIN (SELECT * FROM articlecluster WHERE "end" - begin = '1 day') ac1
+                                      ON acl1.article_cluster_id = ac1.id)
+                
+                    SELECT a.url, a.published_at AS date, a.title, s.name AS source
+                    FROM article a
+                    JOIN source s on s.id = a.source_id
+                    JOIN
+                    (SELECT DISTINCT onedayclusters.article_id
+                    FROM onedayclusters
+                    JOIN
+                    articleclusterlink a2 ON onedayclusters.article_cluster_id = a2.article_cluster_id
+                    WHERE a2.article_id = %(id)s) b ON a.id = b.article_id
+                    AND a.id != %(id)s
+                    ORDER BY added_at DESC;
+                """, found_article)
+                rows = await acur.fetchall()
+                return rows
+            else:
+                return JSONResponse(status_code=404, content={"message": "The submitted URL was not found."})
