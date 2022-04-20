21d877ac79090404ee31701403709b552d6bb56c
Raaid <raaid@protonmail.com>
2022-04-20 16:21:00 +0100

    Add logging (#88)
    * gross but works
    
    * update tests a bit
    
    * name secret conn appropriately; needs prod to run against real data for now

--- api/tests/it/test_api.py
+++ api/tests/it/test_api.py
@@ -11,21 +11,22 @@ client = TestClient(app)
 
 
 def test_search_returns_results_if_similar_articles():
-    body = {"url": "https://www.foxnews.com/world/russian-invades-ukraine-largest-europe-attack-wwii"}
+    body = {"url": "https://www.foxnews.com/world/russian-invades-ukraine-largest-europe-attack-wwii",
+            "client_id": "test"}
     response = client.post(ENDPOINT, json=body)
     assert response.status_code == 200
     assert len(response.json()) != 0
 
 
 def test_search_returns_empty_if_no_similar_articles():
-    body = {"url": "https://www.nytimes.com/live/2022/02/24/world/russia-attacks-ukraine"}
+    body = {"url": "https://www.nytimes.com/live/2022/02/24/world/russia-attacks-ukraine", "client_id": "test"}
     response = client.post(ENDPOINT, json=body)
     assert response.status_code == 200
     assert response.json() == []
 
 
 def test_search_returns_404_if_article_not_found():
-    body = {"url": "https://www.fake.com/this/does-not-exist"}
+    body = {"url": "https://www.fake.com/this/does-not-exist", "client_id": "test"}
     response = client.post(ENDPOINT, json=body)
     assert response.status_code == 404
     assert response.json() == {"message": "The submitted URL was not found."}
